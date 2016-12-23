4e6a01afc77f521e599964f6bb7f2f82bef3571c
bartlomiej.kierys <bskierys@gmail.com>
2016-12-23 22:06:12 +0100

    (Issue #12) Simple copied implementation of chrome custom tabs

---
+++ app/src/main/java/pl/ipebk/tabi/presentation/ui/details/ServiceConnectionCallback.java
@@ -0,0 +1,12 @@
+/*
+* author: Bartlomiej Kierys
+* date: 2016-12-23
+* email: bskierys@gmail.com
+*/
+package pl.ipebk.tabi.presentation.ui.details;
+
+/**
+ * TODO: Generic description. Replace with real one.
+ */
+public class ServiceConnectionCallback {
+}
