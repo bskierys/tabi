16af3003e6e5d50aa9af758e8728eed96f305a60
bartlomiej.kierys <bskierys@gmail.com>
2017-01-30 20:48:34 +0100

    (Issue #38) Established simple layout

--- app/src/main/java/pl/ipebk/tabi/presentation/ui/feedback/FeedbackEntryActivity.java
+++ app/src/main/java/pl/ipebk/tabi/presentation/ui/feedback/FeedbackEntryActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,6 +15,7 @@ import pl.ipebk.tabi.presentation.ui.base.BaseActivity;
 
 public class FeedbackEntryActivity extends BaseActivity {
     @BindView(R.id.edit_issue) EditText issueEntry;
+    @BindView(R.id.txt_title) TextView titleBar;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,25 +24,31 @@ public class FeedbackEntryActivity extends BaseActivity {
 
         int issueType = getIntent().getIntExtra(FeedbackTypeActivity.PARAM_ISSUE_TYPE, 0);
         String hint;
+        String title;
         switch (issueType) {
             case FeedbackTypeActivity.ISSUE_TYPE_BUG: {
                 hint = "Z czym masz problem?";
+                title = "Zgłoś nam swój problem";
                 break;
             }
             case FeedbackTypeActivity.ISSUE_TYPE_IDEA: {
                 hint = "Zaproponuj coś od siebie...";
+                title = "Zgłoś nam swój pomysł";
                 break;
             }
             case FeedbackTypeActivity.ISSUE_TYPE_QUESTION: {
                 hint = "O co chesz zapytać?";
+                title = "Zadaj nam swoje pytanie";
                 break;
             }
             default: {
                 hint = "brak tekstu";
+                title = "brak tekstu";
                 break;
             }
         }
         issueEntry.setHint(hint);
+        titleBar.setText(title);
     }
 
     @Override public void onBackPressed() {
@@ -63,5 +71,6 @@ public class FeedbackEntryActivity extends BaseActivity {
         returnIntent.putExtra(FeedbackTypeActivity.PARAM_ISSUE_RESULT, issueEntryText);
         setResult(Activity.RESULT_OK, returnIntent);
         finish();
+        overridePendingTransition(0,0);
     }
 }
